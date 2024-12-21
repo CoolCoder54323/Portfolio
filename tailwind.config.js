4afba75f6b10df9b95f1aae334a5e30f97a8b8f3
nicholasdixon <nicxellent@icloud.com>
2024-12-21 10:02:30 -0500

    updated configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,9 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        'green': '0px 0px 16px 3px rgba(46,255,157,0.2)',
-        'whiteG': '0px 0px 5px 1px rgba(0,0,0,1)',
+        'greenG': '0px 0px 16px 3px rgba(46,255,157,0.2)',
+        'blackG': '0px 0px 5px 1px rgba(0,0,0,1)',
+        'whiteG': '0px 0px 5px 1px rgba(255,255,255,1)',
 
       }
     },
