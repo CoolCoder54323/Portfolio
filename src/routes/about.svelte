95a66b9d3cf1f257592ed7109941827ed28ef61b
nicholasdixon <nicxellent@icloud.com>
2024-12-21 10:03:46 -0500

    replace placeholder text

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,12 +1,9 @@
-<div class=" bg-gradient-to-br from-slate-900 to-slate-950 text-white p-4 text-center h-screen justify-center flex">
-    <div class="mb-5 w-[32rem] mt-24">
-        <h1 class="text-4xl font-bold  mb-6">About me</h1>
+<div class=" bg-gradient-to-bl from-slate-900 to-slate-950 text-white p-4 text-center h-screen justify-center flex">
+    <div class="mb-auto w-[32rem] mt-auto">
+        <h1 class="text-4xl font-bold  mb-6">About Me (unfinished)</h1>
         <p class=" text-xl text-slate-400"> 
-            I’m a Sophmore at NJIT with a
-                passion for Software development. 
-                With experience in [specific skills or industries, e.g., 'full-stack web development and project management'],
-                I thrive on tackling challenges and learning new things. Outside of work, I enjoy [personal interests or hobbies, e.g., 
-                'hiking, photography, and exploring new cuisines'].
+            I am a Sophomore at NJIT 🎓 with a passion for Software Development 💻. <br><br>
+            With experience in building <span class="text-yellow-400">full-stack </span> web apps and in <span class="text-yellow-400">machine learning</span> 🤖, I thrive on tackling challenges and learning new things! 🚀
 
         </p>
     </div>
