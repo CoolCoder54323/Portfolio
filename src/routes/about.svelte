300bad4cb2c8f3c400ef3df19c86d29ccd2d4ef6
nicholasdixon <nicxellent@icloud.com>
2024-12-30 21:14:35 -0500

    finished about paragraph

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,10 +1,10 @@
 <div class=" bg-gradient-to-bl from-slate-900 to-slate-950 text-white p-4 text-center h-screen justify-center flex">
     <div class="mb-auto w-[32rem] mt-auto">
-        <h1 class="text-4xl font-bold  mb-6">About Me (unfinished)</h1>
+        <h1 class="text-4xl font-bold  mb-6">About Me</h1>
         <p class=" text-xl text-slate-400"> 
-            I am a Sophomore at NJIT 🎓 with a passion for Software Development 💻. <br><br>
+            My name is <span class="text-yellow-400">Nicholas Dixon</span>, I am a Sophomore at the New Jersey Institute of Technology with a passion for Software Development 💻. <br><br>
             With experience in building <span class="text-yellow-400">full-stack </span> web apps and in <span class="text-yellow-400">machine learning</span> 🤖, I thrive on tackling challenges and learning new things! 🚀
-
+            <br><br>When im not coding I apreciate a good game of ping pong 🏓 to unwind, sharpen my reflexes, and enjoy some friendly competition
         </p>
     </div>
 </div>
